85ca1c6bb72eef7a3e2ed4a81723b21ea7f02224
seema777 <navaghareseema3@gmail.com>
2020-08-08 01:21:14 +1300

    Employee Wage Computation

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,21 +1,3 @@
 #!/bin/bash -x
 
-echo "Welcome to Employee Wage Computation Program on Master branch"
-
-
-public class EmpwageBuilder
-{
-        public static void main(String[] args)
-        {
-                int IS_FULL_TIME=1;
-                double empcheck=Math.floor(Math.random()*10)%2;
-                if(empcheck == IS_FULL_TIME)
-                        {
-                                System.out.println("Employee is Persent");
-                        }
-                        else
-                                System.out.println("Employee is Absent");
-        }
-}
-
-
+echo "Welcome to Employee Wage Computation Program on Master Branch"
