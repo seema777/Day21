431b0030d569e0037d90336a24137b0d8c23112f
seema777 <navaghareseema3@gmail.com>
2020-08-08 01:13:38 +1300

    Employee Attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,3 +2,20 @@
 
 echo "Welcome to Employee Wage Computation Program on Master branch"
 
+
+public class EmpwageBuilder
+{
+        public static void main(String[] args)
+        {
+                int IS_FULL_TIME=1;
+                double empcheck=Math.floor(Math.random()*10)%2;
+                if(empcheck == IS_FULL_TIME)
+                        {
+                                System.out.println("Employee is Persent");
+                        }
+                        else
+                                System.out.println("Employee is Absent");
+        }
+}
+
+
